ef3f352fc9fed2a405a96fea1de2b229caf5cd8b
Skullheadx <admonty1@gmail.com>
2024-10-11 21:57:36 -0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -24,49 +24,47 @@ with open("Products A-Z _ Product Suites _ Micro Focus _ OpenText.html", "r") as
     # get to the information
     skip_lines(4)
     
-
-    # Parse product name and demo url:
-    line = f.readline().split() # last element will be the url, everything else is the name of product
-    demo_url = line[-1][1:-1] # slice the first and last character which is < or >
-    line.pop()
-    product_name = " ".join(line)
-    print(product_name)
-    print(demo_url)
-
-    # get to desc
-    skip_lines(1)
-    desc = ""
-    while (True):
-        line = f.readline()
-        if "Get free trial" in line or "Request a demo" in line:
-            break
-        desc += line
-    desc = desc.rstrip() # remove the trailing newlines
-    print(desc)
-
-
-    community_url = ""
-    support_url = ""
-    while(True):
-        line = f.readline()
-        if "Community" in line:
-            community_url = line.split()[-1][1:] # remove the first character
-            community_url += f.readline()[:-4] # remove the "> |" part at the end 
-        elif "Support" in line:
-            support_url = line.split()[-1][1:] # remove the first character
-            support_url += f.readline()[:-4] # remove the "> |" part at the end 
+    for i in range(53): # website says that there are 53 products
         
-        if community_url != "" and support_url != "":
-            break
+        # Parse product name and demo url:
+        line = f.readline().split() # last element will be the url, everything else is the name of product
+        demo_url = line[-1][1:-1] # slice the first and last character which is < or >
+        line.pop()
+        product_name = " ".join(line)
 
-    print(community_url)
-    print(support_url)
+        # get to desc
+        skip_lines(1)
+        desc = ""
+        while (True):
+            line = f.readline()
+            if "Get free trial" in line or "Request a demo" in line:
+                break
+            desc += line
+        desc = desc.rstrip() # remove the trailing newlines
 
 
+        community_url = ""
+        support_url = ""
+        while(True):
+            line = f.readline()
+            if "Community" in line:
+                community_url = line.split()[-1][1:] # remove the first character
+                community_url += f.readline()[:-4] # remove the "> |" part at the end 
+            elif "Support" in line:
+                support_url = line.split()[-1][1:] # remove the first character
+                support_url += f.readline()[:-4] # remove the "> |" part at the end 
+            
+            if community_url != "" and support_url != "":
+                break
 
-
-
-
-    
+        product = {
+            "Product Name": product_name,
+            "Starting Letter": product_name[0].upper(),
+            "Description": desc,
+            "Demo Request URL": demo_url,
+            "Support Link URL": support_url,
+            "Community URL": community_url
+        }
+        print(product)
 
 
